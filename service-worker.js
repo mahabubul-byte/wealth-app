b4cc5737e5764108fd9f7163833c4290b687dc68
mahabubul-byte <pureit.mahabubul@gmail.com>
2026-02-02 16:29:10 +0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,10 @@
-const CACHE_NAME = "wealth-app-v2";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "https://cdn.jsdelivr.net/npm/chart.js"
-];
+const CACHE_NAME = "wealth-app-v3";
+const assets = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
